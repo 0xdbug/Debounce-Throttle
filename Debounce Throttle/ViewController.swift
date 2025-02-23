48752ad0fcefed4cef5bd80589c45167b945b344
dyari <01di4r@gmail.com>
2025-02-23 21:28:51 +0300

    good enough

--- Debounce Throttle/ViewController.swift
+++ Debounce Throttle/ViewController.swift
@@ -11,20 +11,27 @@ import RxCocoa
 import RxGesture
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var draggableView: UIView!
+    
+    @IBOutlet weak var throttleStack: UIStackView!
+    @IBOutlet weak var debounceStack: UIStackView!
+    @IBOutlet weak var regularStack: UIStackView!
     
     let disposeBag = DisposeBag()
     
-    var change = BehaviorRelay<Int>(value: 0)
+    let change = PublishSubject<Void>()
     
-    var regular = BehaviorRelay<Int>(value: 0)
-    var debounce = BehaviorRelay<Int>(value: 0)
-    var throttle = BehaviorRelay<Int>(value: 0)
+    var regular = 0
+    var debounce = 0
+    var throttle = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        prepare(superview: regularStack)
+        prepare(superview: debounceStack)
+        prepare(superview: throttleStack)
         
         let panGesture = draggableView.rx
             .panGesture()
@@ -33,34 +40,79 @@ class ViewController: UIViewController {
         panGesture
             .when(.began, .changed)
             .asTranslation()
-            .subscribe(onNext: { translation, _ in
-                self.draggableView.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
-                self.change.accept(self.change.value+1)
+            .subscribe(onNext: { [self] translation, _ in
+                draggableView.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+                
+                if regular >= regularStack.arrangedSubviews.count - 1 {
+                    regular = 0
+                    clearStack(stack: regularStack)
+                } else {
+                    regular += 1
+                }
+                
+                if debounce >= debounceStack.arrangedSubviews.count - 1 {
+                    debounce = 0
+                    clearStack(stack: debounceStack)
+                } else {
+                    debounce += 1
+                }
+                
+                if throttle >= throttleStack.arrangedSubviews.count - 1 {
+                    throttle = 0
+                    clearStack(stack: throttleStack)
+                } else {
+                    throttle += 1
+                }
+                
+                change.onNext(())
             })
             .disposed(by: disposeBag)
         
-
-//        change.subscribe(onNext: { change in
-//            print("\(change)")
-//        })
-//        .disposed(by: disposeBag)
-        
-        change
-            .throttle(.microseconds(10_0000), scheduler: MainScheduler.asyncInstance)
-            .subscribe(onNext: { [self] change in
-                throttle.accept(throttle.value+1)
+        change.subscribe(onNext: { [self] _ in
+            if regular < regularStack.arrangedSubviews.count {
+                regularStack.arrangedSubviews[regular].isHidden = false
+            }
         })
         .disposed(by: disposeBag)
         
         change
-            .debounce(.microseconds(0), scheduler: MainScheduler.asyncInstance)
-            .subscribe(onNext: { [self] change in
-                debounce.accept(debounce.value+1)
+            .debounce(.microseconds(50000), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [self] _ in
+                if debounce < debounceStack.arrangedSubviews.count {
+                    debounceStack.arrangedSubviews[debounce].isHidden = false
+                }
             })
             .disposed(by: disposeBag)
         
+        change
+            .throttle(.microseconds(50000), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [self] _ in
+                if throttle < throttleStack.arrangedSubviews.count {
+                    throttleStack.arrangedSubviews[throttle].isHidden = false
+                }
+            })
+            .disposed(by: disposeBag)
     }
-
-
+    
+    func prepare(superview: UIStackView) {
+        for i in 0..<30 {
+            let view = UIView()
+            view.backgroundColor = .red
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.isHidden = true
+            
+            superview.addArrangedSubview(view)
+            
+            NSLayoutConstraint.activate([
+                view.heightAnchor.constraint(equalTo: superview.heightAnchor),
+                view.widthAnchor.constraint(equalToConstant: 2)
+            ])
+        }
+    }
+    
+    func clearStack(stack: UIStackView) {
+        for view in stack.arrangedSubviews {
+            view.isHidden = true
+        }
+    }
 }
-
